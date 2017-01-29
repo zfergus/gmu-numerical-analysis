566268591534c0a89d67f00010ebb249eaa25410
Zachary Ferguson <zfergus2@users.noreply.github.com>
2017-01-29 18:21:28 -0500

    Fixed matlab code

--- Project01/bisection_method.m
+++ Project01/bisection_method.m
@@ -4,7 +4,9 @@
 
 % Solve the project questions.
 function bisection_method()
-    %%%%%%
+    fprintf('Bisection Method\nWrtten by Zachary Ferguson\n\n')
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q1a
     fprintf('Q1a:\n\tf(x) = x^3 - 9\n');
     f = @(x) x^3 - 9;
@@ -12,116 +14,121 @@ function bisection_method()
     fprintf('\tr = %.10f\n', r);
     y = 3^(2. / 3.);
     print_errors(f, r, y);
-
-    %%%%%%
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q1b
-    fprintf('Q1b\n\tf(x) = 3x^3 + x^2 - x - 5:\n');
+    fprintf('Q1b:\n\tf(x) = 3x^3 + x^2 - x - 5\n');
     f = @(x) 3 * x^3 + x^2 - x - 5;
     r = compute_root(f, 1, 2);
-    fprintf('r = %.10f\n', r);
-    y = (1. / 9. * (-1 + (593 - 27 * (481^0.5))^(1.0 / 3.0) + (593 + 27 * (481^0.5))^(1.0 / 3.0)));
+    fprintf('\tr = %.10f\n', r);
+    y = (1. / 9. * (-1 + (593 - 27 * (481^0.5))^(1.0 / 3.0) + ...
+        (593 + 27 * (481^0.5))^(1.0 / 3.0)));
     print_errors(f, r, y);
 
-    %%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q1c
-    fprintf('\nf(x) = cos^2(x) - x + 6:\n');
+    fprintf('Q1c:\n\tf(x) = cos^2(x) - x + 6\n');
     f = @(x) cos(x) * cos(x) - x + 6;
     r = compute_root(f, 6, 7);
-    fprintf('r = %.10f\n', r);
+    fprintf('\tr = %.10f\n', r);
     y = 6.7760923163195023262;
     print_errors(f, r, y);
 
-    %%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q3a
-    fprintf('\nf(x) = 2x^3 - 6x - 1:\n');
+    fprintf('Q3a:\n\tf(x) = 2x^3 - 6x - 1\n');
     f = @(x) 2.0 * x^3 - 6 * x - 1;
 
     r = compute_root(f, -2, -1);
-    fprintf('r1 = %.10f\n', r);
+    fprintf('\tr1 = %.10f\n', r);
     y = -1.64178352745293;
     print_errors(f, r, y);
 
     r = compute_root(f, -1, 0);
-    fprintf('r2 = %.10f\n', r);
+    fprintf('\tr2 = %.10f\n', r);
     y = -0.168254401781027;
     print_errors(f, r, y);
 
     r = compute_root(f, 1, 2);
-    fprintf('r3 = %.10f\n', r);
+    fprintf('\tr3 = %.10f\n', r);
     y = 1.81003792923395;
     print_errors(f, r, y);
 
-    %%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q3b
-    fprintf('\nf(x) = e^(x-2) + x^3 - x:\n');
+    fprintf('Q3b:\n\tf(x) = e^(x-2) + x^3 - x\n');
     f = @(x) exp(x - 2) + x^3 - x;
 
     r = compute_root(f, -2, -1);
-    fprintf('r1 = %.10f\n', r);
+    fprintf('\tr1 = %.10f\n', r);
     y = -1.0234821948582364944;
     print_errors(f, r, y);
 
     r = compute_root(f, -0.5, 0.5);
-    fprintf('r2 = %.10f\n', r);
+    fprintf('\tr2 = %.10f\n', r);
     y = 0.16382224325010849634;
     print_errors(f, r, y);
 
     r = compute_root(f, 0.5, 1.5);
-    fprintf('r3 = %.10f\n', r);
+    fprintf('\tr3 = %.10f\n', r);
     y = 0.78894138905554556637;
     print_errors(f, r, y);
 
-    %%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q3c
-    fprintf('\nf(x) = 1 + 5x - 6x^3 - e^(2x):\n');
+    fprintf('Q3c:\n\tf(x) = 1 + 5x - 6x^3 - e^(2x)\n');
     f = @(x) 1 + 5 * x - 6 * x^3 - exp(2 * x);
 
     r = compute_root(f, -1.5, -0.5);
-    fprintf('r1 = %.10f\n', r);
+    fprintf('\tr1 = %.10f\n', r);
     y = -0.81809373448119542124;
     print_errors(f, r, y);
 
     r = compute_root(f, -0.6, 0.4);
-    fprintf('r2 = %.10f\n', r);
+    fprintf('\tr2 = %.10f\n', r);
     y = 0.0;
     print_errors(f, r, y);
 
     r = compute_root(f, 0.5, 1.5);
-    fprintf('r3 = %.10f\n', r);
+    fprintf('\tr3 = %.10f\n', r);
     y = 0.50630828634622119599;
     print_errors(f, r, y);
 
-    %%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Q4a
-    fprintf('\nf(x) = x^2 - A:\n');
-    fprintf('A = 2, (a, b) = (1, 2), n = 22\n');
+    fprintf('Q4a:\n\tf(x) = x^2 - A\n');
+    fprintf('\tA = 2, (a, b) = (1, 2)\n');
     A = 2;
     f = @(x) x^2 - A;
     r = compute_root(f, 1, 2);
-    fprintf('r = %.10f\n', r);
+    fprintf('\tr = %.10f\n', r);
     y = 2^0.5;
     print_errors(f, r, y);
 
-    fprintf('\nA = 3, (a, b) = (1, 2), n = 22\n');
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Q4c
+    fprintf('Q4b:\n\tA = 3, (a, b) = (1, 2)\n');
     A = 3;
     f = @(x) x^2 - A;
     r = compute_root(f, 1, 2);
-    fprintf('r = %.10f\n', r);
+    fprintf('\tr = %.10f\n', r);
     y = 3^0.5;
     print_errors(f, r, y);
-
-    fprintf('\nA = 5, (a, b) = (2, 3), n = 22');
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Q4c
+    fprintf('Q4c:\n\tA = 5, (a, b) = (2, 3)\n');
     A = 5;
     f = @(x) x^2 - A;
     r = compute_root(f, 2, 3);
-    fprintf('r = %.10f\n', r);
+    fprintf('\tr = %.10f\n', r);
     y = 5^0.5;
     print_errors(f, r, y);
 end
 
 
-% Finds the root of the function, f, in the interval [a, b] within an absolute
-% tolerance.
+% Finds the root of the function, f, in the interval [a, b] within an 
+% absolute tolerance.
 function r = compute_root(f, a, b, tol)
     if nargin < 4
         tol = 1e-7;
@@ -142,13 +149,13 @@ function r = compute_root(f, a, b, tol)
         end
         n = n + 1;
     end
-    fprintf('%d\n', n);
+    fprintf('\tn = %d\n', n);
     r = (a + b) / 2.0;
 end
 
 
 % Print the forward and backward error of r.
 function print_errors(f, r, y)
-    fprintf('Forward error: %.10f\n', abs(y - r))
-    fprintf('Backward error: %.10f\n', abs(f(r)))
+    fprintf('\tForward error: %.10f\n', abs(y - r))
+    fprintf('\tBackward error: %.10f\n', abs(f(r)))
 end
--- Project01/fixed_point_iteration.m
+++ Project01/fixed_point_iteration.m
@@ -3,38 +3,36 @@
 
 
 function fixed_point_iteration
-    fprintf('Fixed Point Iteration\nWrtten by Zachary Ferguson\n');
+    fprintf('Fixed Point Iteration\nWrtten by Zachary Ferguson\n\n');
 
-    fprintf('Q1a:\n\tg(x) = (2x+2)^(1/3) = x');
-    fprintf('\txc = %.10f', compute_fixed_point(lambda x: (2 * x + 2)**(1 / 3.),
-        2))
+    fprintf('Q1a:\n\tg(x) = (2x+2)^(1/3) = x\n');
+    fprintf('\txc = %.10f\n', compute_fixed_point(...
+        @(x)(2 * x + 2)^(1 / 3.), 2));
 
-    from math import log
-    fprintf('Q1b:\n\tg(x) = ln(7-x) = x')
-    fprintf('\txc = %.10f', compute_fixed_point(lambda x: log(7 - x), 2))
+    fprintf('Q1b:\n\tg(x) = ln(7-x) = x\n');
+    fprintf('\txc = %.10f\n', compute_fixed_point(@(x) log(7 - x), 2));
+    
+    fprintf('Q1c:\n\tg(x) = ln(4-sin(x)) = x\n');
+    fprintf('\txc = %.10f\n', compute_fixed_point(@(x) log(4 - sin(x)), 2));
 
-    from math import sin
-    fprintf('Q1c:\n\tg(x) = ln(4-sin(x)) = x')
-    fprintf('\txc = %.10f', compute_fixed_point(lambda x: log(4 - sin(x)), 2))
+    A = 3.;
+    g = @(x) (x + A / x) / 2.;
+    fprintf('Q3a:\n\tg(x) = (x + 3 / x) / 2\n');
+    fprintf('\tx0 = 2\n');
+    fprintf('\txc = %.10f\n', compute_fixed_point(g, 2));
 
-    A = 3.
-    g = lambda x: (x + A / x) / 2.
-    fprintf('Q3a:\n\tg(x) = (x + 3 / x) / 2')
-    fprintf('\tx0 = 2')
-    fprintf('\txc = %.10f', compute_fixed_point(g, 2, print_n=True))
+    A = 5.;
+    g = @(x) (x + A / x) / 2.;
+    fprintf('Q3b:\n\tg(x) = (x + 5 / x) / 2\n');
+    fprintf('\tx0 = 2\n');
+    fprintf('\txc = %.10f\n', compute_fixed_point(g, 2));
 
-    A = 5.
-    fprintf('Q3b:\n\tg(x) = (x + 5 / x) / 2')
-    fprintf('\tx0 = 2')
-    fprintf('\txc = %.10f', compute_fixed_point(g, 2, print_n=True))
-
-    from math import cos
-    fprintf('Q5:\n\tg(x) = cos^2(x)')
-    xc = compute_fixed_point(lambda x: (cos(x))**2, 1, tol=1e-6, print_n=True)
-    fprintf('\txc = %.10f', xc)
-    fprintf('\n\td/dx g(x) = -2*cos(x)*sin(x)')
-    fprintf('\t|d\dx g(xc)| = %.10f', abs(-2 * cos(xc) * sin(xc)))
-    fprintf('\tTherefore g(x) is locally convergent to xc.')
+    fprintf('Q5:\n\tg(x) = cos^2(x)\n');
+    xc = compute_fixed_point(@(x) (cos(x))^2, 1, 1e-6);
+    fprintf('\txc = %.10f\n', xc);
+    fprintf('\n\td/dx g(x) = -2*cos(x)*sin(x)\n');
+    fprintf('\t|d/dx g(xc)| = %.10f\n', abs(-2 * cos(xc) * sin(xc)));
+    fprintf('\tTherefore g(x) is locally convergent to xc.\n');
 end
 
 
@@ -50,9 +48,8 @@ function xc = compute_fixed_point(g, x0, tol)
     while (abs(prev_x - x) > 0.5 * tol)
         prev_x = x;
         x = g(x);
-        n += 1;
+        n = n + 1;
     end
-    fprintf('\tn = %d', n);
-    end
+    fprintf('\tn = %d\n', n);
     xc = x;
 end
