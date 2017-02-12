96c4f3fe8f6d57d5b2eb0afa5aff6308ed3a2e13
Zachary Ferguson <zfergus2@users.noreply.github.com>
2017-02-11 23:53:22 -0500

    Added Report for Proj03

--- Project03/newtons_method.m
+++ Project03/newtons_method.m
@@ -5,44 +5,94 @@
 % Solve the project questions
 function newtons_method
     % Q1a
-    fprintf('Q1a:\n\tf(x) = x^3 - 2x - 2 = 0');
+    fprintf('Q1a:\n\tf(x) = x^3 - 2x - 2 = 0\n');
     f  = @(x) x^3 - 2*x - 2;
     fp = @(x) 3*x^2 - 2;
     x0 = 2;
-    fprintf('\tx0 = %.10f\n', x0);
+    fprintf('\tx0 = %g\n', x0);
     fprintf('\tr = %.10f\n', compute_root(f, fp, x0));
 
     % Q1b
-    fprintf('Q1b:\n\tf(x) = e^x + x - 7 = 0');
+    fprintf('Q1b:\n\tf(x) = e^x + x - 7 = 0\n');
     f  = @(x) exp(x) + x - 7;
     fp = @(x) exp(x) + 1;
     x0 = 0;
-    fprintf('\tx0 = %.10f\n', x0);
+    fprintf('\tx0 = %g\n', x0);
     fprintf('\tr = %.10f\n', compute_root(f, fp, x0));
 
     % Q1c
-    fprintf('Q1c:\n\tf(x) = e^x + sin(x) - 4 = 0');
-    f  = @(x) exp(x) - sin(x) - 4;
-    fp = @(x) exp(x) - cos(x);
+    fprintf('Q1c:\n\tf(x) = e^x + sin(x) - 4 = 0\n');
+    f  = @(x) exp(x) + sin(x) - 4;
+    fp = @(x) exp(x) + cos(x);
     x0 = 2;
-    fprintf('\tx0 = %.10f\n', x0);
+    fprintf('\tx0 = %g\n', x0);
     fprintf('\tr = %.10f\n', compute_root(f, fp, x0));
+
+    % Q3a
+    fprintf('Q3a:\n\tf(x) = 27x^3 + 54x^2 + 36x + 8 = 0\n');
+    f  = @(x) 27*x^3 + 54*x^2 + 36*x + 8;
+    fp = @(x) 81*x^2 + 108*x + 36;
+    x0 = 0.0;
+    r = -2/3;
+    fprintf('\tx0 = %g\n', x0);
+    xc = compute_root(f, fp, x0, 1e-16);
+    fprintf('\txc = %.16f\n', xc);
+    fprintf('\tForward Error = |r - xc| = %.16f\n', abs(r-xc));
+    fprintf('\tBackward Error = f(xc) = %.16f\n', f(xc));
+    fprintf('\tmultiplicity of r is 3\n');
+    xc = compute_root(f, fp, x0, 1e-16, 3);
+    fprintf('\txc = %.16f\n', xc);
+    fprintf('\tForward Error = |r - xc| = %.16f\n', abs(r-xc));
+    fprintf('\tBackward Error = f(xc) = %.16f\n', f(xc));
+
+    % Q9
+    fprintf('Q9:\n\tf(x) = 14xe^(x-2) - 12e^(x-2) - 7x^3 + 20x^2 - 26x + 12\n');
+    f  = @(x) 14*x*exp(x-2) - 12*exp(x-2) - 7*x^3 + 20*x^2 - 26*x + 12;
+    fp = @(x) 14*x*exp(x-2) + 2*exp(x-2) - 21*x^2 + 40*x - 26;
+    fpp = @(x) 14*x*exp(x-2) + 4*exp(x-2) - 42*x + 40
+    x0 = 0;
+    r = compute_root(f, fp, x0, 1e-9, 1, 2);
+    fprintf('\tr1 = %.10f\n', r);
+    fprintf('\tM = lim i->inf (e_(i+1)/(e_i)^2) = %.10f\n\n', ...
+        abs(fpp(r)/(2*fp(r))))
+
+    x0 = 3.0;
+    r = compute_root(f, fp, x0, 1e-9, 1, 1);
+    fprintf('\tr2 = %.10f\n', r);
+    fprintf('\tmultiplicity of r2 is 3 -> ')
+    fprintf('S = lim i->inf (e_(i+1)/e_i) = %.10f\n', 2/3)
 end
 
 
 % Compute the foot to f(x)
-function r = compute_root(f, fp, x0, tol)
+function r = compute_root(f, fp, x0, tol, m, print_ei)
     if nargin < 4
         tol = 1e-9;
     end
+    if nargin < 5
+        m = 1;
+    end
+    if nargin < 6
+        print_ei = 0;
+    end
+
+    r = fzero(f, x0);
 
     n = 0;
     x = x0;
-    while (abs(f(x)) > 0.5 * tol)
-        x = x - f(x) / fp(x);
-        n = n + 1
+    ei = 1;
+    ei_1 = 1;
+    while (abs(f(x)) >= 0.5 * tol)
+        x = x - m * f(x) / fp(x);
+        n = n + 1;
+        ei_1 = ei;
+        ei = abs(r - x);
+        if print_ei == 1
+            fprintf('\te_i = %.8f; e_(i+1)/e_i = %.8f\n', ei, ei/ei_1);
+        elseif print_ei == 2
+            fprintf('\te_i = %.8f; e_(i+1)/(e_i)^2 = %.8f\n', ei, ei/(ei_1^2));
+        end
     end
     fprintf('\tn = %d\n', n)
-    r = x
+    r = x;
 end
-df(x0)
---
+++ Project03/report.md
@@ -0,0 +1,83 @@
+# MATH 446: Project 02
+#### Zachary Ferguson
+
+## Contents
+
+1. Questions
+2. Code
+3. Output
+
+## Questions
+
+### Q1.
+#### a.
+\begin{equation}
+\begin{matrix}
+f(x) = x^3 - 2x - 2 = 0 \\
+r = 1.76929235
+\end{matrix}
+\end{equation}
+
+#### b.
+\begin{equation}
+\begin{matrix}
+f(x) = e^x + x - 7 = 0 \\
+r = 1.6728216986
+\end{matrix}
+\end{equation}
+
+#### c.
+\begin{equation}
+\begin{matrix}
+f(x) = e^x + sin(x) - 4 = 0
+r = 1.1299804987
+\end{matrix}
+\end{equation}
+
+### Q3.
+#### a.
+\begin{equation}
+f(x) = 27x^3 + 54x^2 + 36x + 8 = 0
+\end{equation}
+\begin{equation}
+f'(x) = 81x^2 + 108x + 36
+\end{equation}
+\begin{equation}
+f''(x) = 162x + 108
+\end{equation}
+\begin{equation}
+f'''(x) = 162 /= 0
+\end{equation}
+\begin{equation}
+\begin{matrix}
+r = {-2 \ over 3} \\
+f(r) = f'(r) = f''(r) = 0 \rightarrow \text{multiplicity of } r \text{ is } 3
+\end{matrix}
+\end{equation}
+
+### Q9.
+f(x)  = 14xe^{x-2} - 12e^{x-2} - 7x^3 + 20x^2 - 26x + 12
+f'(x) = 14xe^{x-2} + 2e^{x-2} - 21x^2 + 40x - 26
+f''(x) = 14xe^{x-2} + 16e^{x-2} - 42x + 40
+f'''(x) = 14xe^{x-2} + 30e^{x-2} - 42
+
+r_1 = 0.85714285
+f'(r1) \approx -2.67817 \neq 0 \rightarrow lim_{i \rightarrow \infty}
+    {e_{i+1} \over {e_i}^2} = \left| {f''(r_1) \over {2f'(r_1)}} \right|
+    \approx 1.69939188
+
+r_2 = 2.0
+f(r_2) = f'(r_2) = f''(r_2) = 0 \rightarrow \text{multiplicity of } r_2 \text{ is } 3
+\therefore S = lim_{i \rightarrow \infty} {e_{i+1} \over e_i} = {m-1 \over m} = {2 \over 3}
+
+## Code
+
+```Matlab
+
+```
+
+## Output
+
+```
+
+```
